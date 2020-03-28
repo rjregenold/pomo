777008fc3fc43cd9f0e9b829be4e96862b2e7aa5
RJ Regenold <rjregenold@gmail.com>
2020-03-28 17:18:33 -0500

    refactors the things.

--- src/Page/Home.purs
+++ src/Page/Home.purs
@@ -3,21 +3,32 @@ module Pomo.Page.Home where
 import Prelude
 
 import Control.Monad.Reader (class MonadAsk, ask)
-import Data.Maybe (Maybe(..), maybe)
+import Data.DateTime.Instant (Instant, unInstant)
+import Data.Maybe (Maybe(..))
 import Data.Time.Duration (Milliseconds(..))
 import Effect.Aff (delay)
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Pomo.Capability.Now (class Now, now)
 import Pomo.Data.TimerSettings (TimerSettings)
 
 type State =
   { timerSettings :: Maybe TimerSettings
-  , running :: Boolean
-  , timerForkId :: Maybe H.ForkId
-  , ticks :: Int
+  , timerState :: TimerState
   }
+
+type RunningTimerState =
+  { startedAt :: Instant
+  , forkId :: H.ForkId
+  , currentTime :: Instant 
+  }
+
+data TimerState
+  = NotRunning
+  | Running RunningTimerState
 
 data Action
   = Init
@@ -28,14 +39,13 @@ component
   :: forall q m r
    . MonadAff m
   => MonadAsk { timerSettings :: TimerSettings | r } m
+  => Now m
   => H.Component HH.HTML q {} Void m
 component = 
   H.mkComponent
     { initialState: \_ -> 
         { timerSettings: Nothing
-        , running: false
-        , timerForkId: Nothing
-        , ticks: 0 
+        , timerState: NotRunning
         }
     , render
     , eval: H.mkEval $ H.defaultEval
@@ -50,36 +60,51 @@ component =
       [ HH.h1_ [ HH.text "Pomo" ]
       , HH.h3_ [ HH.text timerLabel ]
       , HH.button
-          [ HE.onClick \_ -> Just ToggleTimer ]
-          [ HH.text $ if state.running then "Stop" else "Start" ]
+          [ HP.title buttonLabel
+          , HE.onClick \_ -> Just ToggleTimer 
+          ]
+          [ HH.text buttonLabel ]
       ]
     where
-    timerLabel = 
-      if state.running
-        then show state.ticks
-        else "25:00"
+    buttonLabel = case state.timerState of
+      NotRunning -> "Start"
+      Running _ -> "Stop"
+    msDiff (Milliseconds a) (Milliseconds b) = a - b
+    timerLabel = case state.timerState of
+      NotRunning -> "25:00"
+      Running ts -> show (msDiff (unInstant ts.currentTime) (unInstant ts.startedAt))
 
   handleAction :: forall slots. Action -> H.HalogenM State Action slots Void m Unit
-  handleAction action = 
-    case action of
-         Init -> do
-            { timerSettings } <- ask
-            H.modify_ _ { timerSettings = Just timerSettings }
-         ToggleTimer -> do
-          st <- H.modify $ \st -> st
-           { running = not st.running
-           , ticks = if st.running then 0 else st.ticks
-           }
-          if st.running
-          then do
-             let loop = do
-                   H.liftAff (delay (Milliseconds 1000.0))
-                   handleAction Tick
-                   loop
-             forkId <- H.fork loop
-             H.modify_ _ { timerForkId = Just forkId }
-          else do
-             _ <- maybe (pure unit) H.kill st.timerForkId
-             H.modify_ _ { timerForkId = Nothing }
-         Tick -> H.modify_ \st -> st { ticks = st.ticks + 1 }
-
+  handleAction action = case action of
+    Init -> do
+      { timerSettings } <- ask
+      H.modify_ _ { timerSettings = Just timerSettings }
+    ToggleTimer -> do
+      st <- H.get
+      case st.timerState of
+        NotRunning -> do
+          currentTime <- now
+          let loop = do
+                H.liftAff (delay (Milliseconds 1000.0))
+                handleAction Tick
+                loop
+          forkId <- H.fork loop
+          H.put $ st
+            { timerState = Running
+              { currentTime: currentTime
+              , startedAt: currentTime
+              , forkId: forkId
+              }
+            }
+        Running ts -> do
+          H.kill ts.forkId
+          H.put $ st
+            { timerState = NotRunning
+            }
+    Tick -> do
+      st <- H.get
+      case st.timerState of
+        NotRunning -> pure unit
+        Running ts -> do
+          currentTime <- now
+          H.put $ st { timerState = Running $ ts { currentTime = currentTime } }
